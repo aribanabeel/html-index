738ebba3bea1065e7aa69d76a2388c4cf93ea767
aribanabeel <aribanabeel27@gmail.com>
2024-06-05 02:27:30 -0700

    index.js

---
+++ js/index.js
@@ -0,0 +1,55 @@
+       // input four numbers and display sum of these 
+       let sum1 = 5;
+       let sum2 = 9;
+       let sum3 = 16;
+       let sum4 = 27;
+       let totalSUM = sum1 + sum2 + sum3 + sum4;
+       console.log( "Total sum is:", totalSUM);
+           // input marks of five subjects of a student and calculate its average 
+        let num1 = 99;
+        let num2 = 44;
+        let num3 = 67;
+        let num4 = 12;
+        let num5 = 86;
+        let average = (num1 + num2 + num3 + num4 + num5)/5;
+        console.log( "Total average of five subject:",average);
+                       //  input radius of a circle and calculate area of circle
+       let radius = 4;
+       const  pi = 3.14;
+       let areaOFCIRCLE = pi * radius **2 ;
+       console.log( " Area of a circle is: ",areaOFCIRCLE, "SQ");
+       // take input from user about temoerature in celsius and display equivalent farhenheit temperature 
+       let celsius = 56;
+       let farhenheit = (celsius * 9/5) + 32;
+       console.log("Fahrenheit temperature is :",farhenheit, "F");
+           // calculate the pay of an employee, given the hours worked and rate per hour 
+           let hours = 10;
+           let rate = 875;
+           let totalpay = hours * rate ;
+           console.log("Pay of an employee:",totalpay, "RS" );
+       // a painter wants to know the amount of paint needed to paint only the four walls. Each wall has same rectangular area. the choosen paint covers 100 square feet per gallon
+        let width1 = 9;
+        let height1 = 17;
+        let areaof1wall = width1 + height1;
+        let areaof4walls = areaof1wall*4;
+        let gallon = 100;
+        let totalpaint = areaof4walls/100;
+        console.log("Total required paint:",totalpaint);
+       // bob would like to know what percentage of his income his rent is. 
+       let income = 57000;
+       let rent = 29000;
+       let percentage = (rent/income) * 100;
+       console.log("Total percentage % :",percentage);
+       //   circumferenc of a circle 
+       let diameter = 5.6;
+       const PIE = 3.14;
+       let circumference = PIE * diameter ;
+       console.log("Circumference of a circle:", circumference);
+                                          // table of 6
+   //    jim is going to replant his lawn. he would like to know how much the sod will cost, given cost of the sod per square feet and the lenght and the width of two different lawn areas in feet.  find cost 
+   let lawn1 = 33*19;
+   let lawn2 = 45*27;
+   let sodcost = 3.8
+   let totallawnarea = lawn1 + lawn2 ;
+   let totalsodcost = totallawnarea * sodcost ;
+   console.log("total sod cost:",totalsodcost)
